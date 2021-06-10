9b5dd38bdc5fbfeef22a307d168df0b78966bc58
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:39:47 +0530

    Add files via upload

---
+++ LINQ Concat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Concat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> sequence1 = new List<int> { 1, 2, 3, 4 };
+            List<int> sequence2 = new List<int> { 2, 4, 6, 8 };
+            var result = sequence1.Concat(sequence2);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
